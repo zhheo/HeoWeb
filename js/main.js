ed9f39669d838743026daacdbc3984c26906f0aa
zhheo <qewan@foxmail.com>
2022-10-23 09:32:52 +0800

    禁止选中、添加图标

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// 添加视差效果
+var image = document.getElementsByClassName('banner-pic-img');
+new simpleParallax(image, {
+  orientation: 'up',
+  scale: 1.3,
+  overflow: true
+});
+
+// 添加菜单点击事件
+var menuButton = document.getElementById("nav-menu");
+menuButton.addEventListener('click',function(){
+  if(document.getElementById("body").classList.contains('show-menu')) {
+    heoWeb.hideMenu();
+  }else {
+    heoWeb.showMenu();
+  }
+},false)
+
+//关闭菜单
+$('.menu-list').click(function () {
+	heoWeb.hideMenu();
+});
+
+//阻止菜单滚动
+document.querySelector('.menu-list').addEventListener('wheel',(e)=>{
+  e.preventDefault()
+})
+
+var heoWeb = {
+  //显示菜单
+  showMenu: function() {
+    document.getElementById("body").classList.add("show-menu");
+  },
+
+  hideMenu: function() {
+    document.getElementById("body").classList.remove("show-menu");
+  },
+
+  //跳转到id
+  scrollTo:function(id){
+    var domTop = document.querySelector(id).offsetTop;
+    window.scrollTo(0,domTop - 60);
+  }
+}
