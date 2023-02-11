c6eba27da71c9b32076c980904b7c55475909a6c
zhheo <qewan@foxmail.com>
2023-02-11 17:43:00 +0800

    支持点击导航栏时滚动到该位置而不是直接跳转到指定位置

--- js/main.js
+++ js/main.js
@@ -40,8 +40,29 @@ var heoWeb = {
   },
 
   //跳转到id
-  scrollTo:function(id){
-    var domTop = document.querySelector(id).offsetTop;
-    window.scrollTo(0,domTop - 60);
+  scrollTo(id) {
+    let target = document.getElementById(id);
+    if (!target) return;
+    let targetPosition = target.offsetTop - 60;
+    let startPosition = window.pageYOffset;
+    let distance = targetPosition - startPosition;
+    let startTime = null;
+    
+    function animation(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      let timeElapsed = currentTime - startTime;
+      let run = ease(timeElapsed, startPosition, distance, 600);
+      window.scrollTo(0, run);
+      if (timeElapsed < 600) requestAnimationFrame(animation);
+    }
+  
+    function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+  
+    requestAnimationFrame(animation);
   }
 }
